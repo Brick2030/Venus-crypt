7be2a56dc6f188564585c072016ae39ec1dfda66
Brick <petrovskajaval@gmail.com>
2023-12-08 01:29:29 +0700

    just few changes. learning python

---
+++ test.py
@@ -0,0 +1,12 @@
+print("Welcome to Protocol7");
+print("A digital \"Cardan grille\" Version 0 \n");
+message = input("Enter the message to encrypt:\n");
+print("Your message is \"" + message +"\"");
+
+# Making uni-char list for dictionary and message
+
+# Dividing string array into words
+
+# We need uni-char list to make function that will find all positions of the choosen char in the dictionary
+
+# Encrypting each words by changing it by each char to random number from position list
