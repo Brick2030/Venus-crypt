11b273b1a7bd830b78eee6f978410bf2b6dd1e56
Brick <petrovskajaval@gmail.com>
2024-01-20 14:12:38 +0700

    code optimization

---
+++ core.py
@@ -0,0 +1,43 @@
+import random
+
+                                # Todo list
+# Checking for missing chars before encryption. Maybe changing missing to random.
+# Dictionary generator
+# Support file systems
+# Quick mode? 
+# Research on input settings
+
+
+def uniCharList(input): # Return unique symbols in givern line. Used for dictionary and message later.
+    uniList = []
+    for chr in input:
+        if(uniList.count(chr)==0): uniList.append(chr)
+        else: continue
+
+    return uniList # Returns array
+
+
+def charIndexList(char, string): # Finds every index of char in string
+    indexList = []
+    for i, val in enumerate(string):
+        if val == char: indexList.append(i)
+    
+    return indexList
+
+def encrypt(message, dictionary, separator): # Returns encrypted messange
+    encryptedMessage = [] # Output
+    for chr in message:
+        thisCharIndexs = charIndexList(chr, dictionary) # All char index in dictionary
+        randomIndex = int(random.uniform(0, len(thisCharIndexs)))
+        encryptedMessage.append(str(thisCharIndexs[int(randomIndex)]))
+
+    return separator.join(encryptedMessage)
+
+def decrypt(key, dictionary, separator): # Decryption
+    keyword = key.split(separator)
+    decryptedMessage = []
+    for number in keyword:
+        decryptedMessage.append(dictionary[int(number)])
+
+    return "".join(decryptedMessage)
+
--- test.py
+++ test.py
@@ -1,63 +1,12 @@
-import random
+import core
 
-#message = input("Enter the message to encrypt:\n")
-message = "Autem numquam maiores et. Iure voluptate enim recusandae cum nihil quisquam. Unde est beatae sint ut dolorum nesciunt. Aut consequatur tenetur"
+message = input("Enter the message to encrypt:\n")
+#message = "Autem numquam maiores et. Iure voluptate enim recusandae cum nihil quisquam. Unde est beatae sint ut dolorum nesciunt. Aut consequatur tenetur"
 message = message.lower()
-dictionary = " abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz"
+dictionary = " abcdefghijklmnopqrstuvwxyz-.abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz-.,()/:;;';labcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz"
 
 
-def uniCharList(input): # Return unique symbols in givern line. Used for dictionary and message later.
-    uniList = []
-    for chr in input:
-        if(uniList.count(chr)==0): uniList.append(chr)
-        else: continue
-
-    return uniList # Returns array
-
-
-
-
-
-def charIndexList(char, string): # Finds every index of char in string
-    indexList = []
-    
-    for i, val in enumerate(string):
-        if val == char: indexList.append(i)
-    return indexList
-
-
-
-def encrypt(message, dictionary, separator): # Returns encrypted messange
-    encryptedMessage = [] # Output
-
-    for chr in message:
-        thisCharIndexs = charIndexList(chr, dictionary) # All char index in dictionary
-        randomIndex = int(random.uniform(0, len(thisCharIndexs)))
-        encryptedMessage.append(str(thisCharIndexs[int(randomIndex)]))
-
-    return separator.join(encryptedMessage)
-
-
-
-
-def decrypt(key, dictionary, separator): # Decryption
-    keyword = key.split(separator)
-    decryptedMessage = []
-    for number in keyword:
-        decryptedMessage.append(dictionary[int(number)])
-
-    return "".join(decryptedMessage)
-
-
-
-
-
-# TESTING ####################################################################
-
-key = encrypt("hello world", dictionary, '-')
+key = core.encrypt(message, dictionary, '|')
 print(key)
-msg = decrypt(key, dictionary, '-')
+msg = core.decrypt(key, dictionary, '|')
 print(msg)
-
-
-# Checking for missing chars before encryption.
